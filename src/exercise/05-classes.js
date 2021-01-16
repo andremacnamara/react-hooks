86d40ec8b8250ce142584138503ea7867ea359ff
Andre MacNamara <andremacnamara12@gmail.com>
2021-01-16 13:05:53 +0000

    useRef, and useEffect dom interaction

--- src/exercise/05-classes.js
+++ src/exercise/05-classes.js
@@ -7,30 +7,56 @@ import VanillaTilt from 'vanilla-tilt'
 
 // If you'd rather practice refactoring a class component to a function
 // component with hooks, then go ahead and do this exercise.
+function Tilt({ children }){
+  const titleRef = React.useRef();
 
-class Tilt extends React.Component {
-  tiltRef = React.createRef()
-  componentDidMount() {
-    const tiltNode = this.tiltRef.current
+  React.useEffect(() => {
+    const titleNode = titleRef.current;
+
     const vanillaTiltOptions = {
       max: 25,
       speed: 400,
       glare: true,
       'max-glare': 0.5,
-    }
-    VanillaTilt.init(tiltNode, vanillaTiltOptions)
-  }
-  componentWillUnmount() {
-    this.tiltRef.current.vanillaTilt.destroy()
-  }
-  render() {
-    return (
-      <div ref={this.tiltRef} className="tilt-root">
-        <div className="tilt-child">{this.props.children}</div>
-      </div>
-    )
-  }
-}
+    };
+
+    VanillaTilt.init(titleNode, vanillaTiltOptions)
+
+    return () => titleNode.vanillaTilt.destroy();
+  }, []);
+
+  return (
+    <div className="tilt-root" ref={titleRef}>
+      <div className="tilt-child">{children}</div>
+    </div>
+  )
+};
+
+
+// class Tilt extends React.Component {
+//   tiltRef = React.createRef()
+//   componentDidMount() {
+//     const tiltNode = this.tiltRef.current
+//     const vanillaTiltOptions = {
+//       max: 25,
+//       speed: 400,
+//       glare: true,
+//       'max-glare': 0.5,
+//     }
+//     VanillaTilt.init(tiltNode, vanillaTiltOptions)
+//   }
+//   componentWillUnmount() {
+//     this.tiltRef.current.vanillaTilt.destroy()
+//   }
+//   render() {
+//     return (
+//       <div ref={this.tiltRef} className="tilt-root">
+//         <div className="tilt-child">{this.props.children}</div>
+//       </div>
+//     )
+//   }
+// }
+
 function App() {
   return (
     <Tilt>
